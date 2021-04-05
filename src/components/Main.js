a498730a994e8944171ac55a29aaaa031b1ffe47
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-04-05 22:51:24 +0200

    added title into main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 const Main = (props) => {
   return (
     <main className="main">
-      <Link to="/EuroList">
-        <button className="main__button">Eurocopa</button>
-      </Link>
-      <Link to="/MundialList">
-        <button className="main__button">Mundial</button>
-      </Link>
+      <h1 className="main__title">Qué te gusta más</h1>
+      <div className="main__container">
+        <Link to="/EuroList">
+          <button className="main__button">Eurocopa</button>
+        </Link>
+        <Link to="/MundialList">
+          <button className="main__button">Mundial</button>
+        </Link>
+      </div>
     </main>
   );
 };
